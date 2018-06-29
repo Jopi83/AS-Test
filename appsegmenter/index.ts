b4bb26d0663e9b479afd154dbcf3a38f80d81e0f
jhetzer <jopi83@hotmail.de>
2018-06-29 13:14:41 +0200

    Angular Tests

--- appsegmenter/index.ts
+++ appsegmenter/index.ts
@@ -1,3 +1,3 @@
-export * from './src/app/modules/sample-component/sample.config';
-export * from './src/app/modules/sample-component/sample.component';
-export * from './src/app/modules/sample-component/sample.service';
+//export * from './src/app/modules/sample-component/sample.config';
+//export * from './src/app/modules/sample-component/sample.component';
+//export * from './src/app/modules/sample-component/sample.service';
